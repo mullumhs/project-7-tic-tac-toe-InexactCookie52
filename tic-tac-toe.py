c6aeac1246a146e11fcd9e5d5543bf87b454ca8a
InexactCookie52 <140124275+InexactCookie52@users.noreply.github.com>
2024-05-02 12:40:24 +1000

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,12 +1,9 @@
 board = []
-#colom_key = {[]}
+colom_key = {"A": 0,"B": 1,"C": 2}
 def initialise_board():
     for i in range(3):
         i += 1
         board.append(["-","-","-", i])
-
-        
-
 def print_board():
     print (" A B C")
     for row in board:
@@ -14,44 +11,53 @@ def print_board():
         for col in row:
             print(col, end='|')
         print()
-def move():
-    player_count = 1
+def move(turncount):
+    
+    player_count = turncount
     while True: 
-        
+        print_board()
         if not player_count % 2 == 0:
             token = 'X'
             print ("player X turn; ")
         if player_count % 2 == 0:
             token = 'O'
             print ("Player O turn")
+        while True:
+            choice_col = (input("please select the colom you would like to place, Either A B or C: ")).upper()
+            while not choice_col in colom_key:
+                #checks if user input is in the dictionary
+                print("invalid input: ")
+                choice_col = (input("please select the colom you would like to place, Either A B or C: ")).upper()
+            while True:
+                choice_row = input("now please select a row to place in, either 1 2 or 3: ")
+                try:    
+                    choice_row = int(choice_row) - 1
+                    if choice_row > -1 and choice_row < 3:
+                    #checks if between 1 and 3
+                        break
+                    else:
+                        print("invalid input: ")
+                except:
+                    print("invalid input: ")
+                # checks if the row choice is a number and if that number is between 1 and 3.
+            
 
-        choice_col = input("please select the colom you would like to place, Either A B or C: ").title
-        if choice_col == 'A':
-            choice_col = 0
-        elif choice_col == 'B':
-            choice_col = 1
-        elif choice_col == 'C':
-            choice_col = 2
-        else:
-            print("invalid input: ")
-
-
-
-
-        choice_row = int(input("now please select a row to place in, either 1 2 or 3: "))
-        choice_row += 1
-        board[choice_row][choice_col] = token
-        """
-        for i in range(5, -1, -1):
-            if board[i][choice] == '-':
-                board[i][choice] = token   
+            if board[choice_row][colom_key[choice_col]] == '-':
+                board[choice_row][colom_key[choice_col]] = token
                 player_count += 1
                 break
-        
-        for row in range(6):
-            for col in row:
-                print()
-"""
-initialise_board()
-move()
-print_board()
+            else:
+                print("this spot is already taken!: ")
+
+        break
+def main():
+    initialise_board()
+    turncount = 1
+    while True:
+        move(turncount)
+        print_board()
+        turncount += 1
+        if turncount == 9:
+            break
+#figure out player count
+main()
